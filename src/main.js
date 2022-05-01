a01a65289004b0505dfdcc129bab1225012c41fb
lxu58 <103284069+lxu58@users.noreply.github.com>
2022-05-01 14:46:11 -0700

    update

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// Main.js holds global variables and essential config datas
+
+let config = {
+    type: Phaser.CANVAS,
+    width: 1280,
+    height: 720,
+    scene: [ Menu_1, Play_solo, Tutorial, Credit]
+}
+
+var game = new Phaser.Game(config);
+var borderUISize = game.config.height/ 20;
+var borderPadding = borderUISize / 3;
+
+// Key inputs
+var keyLeft_P1, keyRight_P1, keyUp_P1, keyDown_P1, keyR;
+var keyA, keyD, keyEnter, keyESC;         //key for menu
+
+// Global variables
+var asteroidCreaded = false;
+var player2 = [];
+var ai = [];
+
+function Vector2(x, y) {
+    return new Phaser.Math.Vector2(x, y);
+}
